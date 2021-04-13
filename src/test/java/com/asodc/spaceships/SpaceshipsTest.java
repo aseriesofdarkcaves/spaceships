293ae88400857988b4a0d0f432f57f9e9c620980
David Ferguson <David.Ferguson@t-systems.com>
2021-04-13 19:55:08 +0200

    Initial commit

---
+++ src/test/java/com/asodc/spaceships/SpaceshipsTest.java
@@ -0,0 +1,10 @@
+package com.asodc.spaceships;
+
+import org.junit.jupiter.api.Test;
+
+public class SpaceshipsTest {
+    @Test
+    public void justPassWithoutDoingAnything() {
+        // not doing anything at the moment!
+    }
+}
