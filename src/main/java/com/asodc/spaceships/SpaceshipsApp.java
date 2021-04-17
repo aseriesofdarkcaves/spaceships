4c17b218b32b3ab870f303c81e4ee32bcf4ee419
aseriesofdarkcaves <aseriesofdarkcaves@gmail.com>
2021-04-17 23:55:55 +0200

    initial work on ship interior PoC

--- src/main/java/com/asodc/spaceships/SpaceshipsApp.java
+++ src/main/java/com/asodc/spaceships/SpaceshipsApp.java
@@ -2,13 +2,21 @@ package com.asodc.spaceships;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
+import com.jme3.math.*;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Quad;
 
 public class SpaceshipsApp extends SimpleApplication {
     private Geometry player;
     private Ship playerShip;
+
+    private static final Vector3f PLAYER_INITIAL_POSITION = new Vector3f(2f, 2f, -2f);
+    private static final Quad FLOOR = new Quad(10f, 20f);
+
+    private SpaceshipsApp() {
+        // don't want multiple instances
+    }
 
     public static void main(String... args) {
         new SpaceshipsApp().start();
@@ -16,20 +24,30 @@ public class SpaceshipsApp extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        initScene();
+        initShipInterior();
         initPlayer();
         initShip();
     }
 
-    private void initScene() {
-        // TODO: set up the inside of the ship
-        // TODO: create a floor
+    private void initShipInterior() {
+        // the floor starts at origin and is rotated so that it becomes a plane on the x-axis
+        Material material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        material.setColor("Color", ColorRGBA.Red);
+
+        Geometry geometry = new Geometry("floor", FLOOR);
+        geometry.setMaterial(material);
+
+        // rotates the plane 90 degrees around the x-axis away from the initial camera
+        geometry.rotate(-FastMath.PI / 2, 0f, 0f);
+
+        rootNode.attachChild(geometry);
+
         // TODO: create walls
+
         // TODO: create ship terminals
     }
 
     private void initPlayer() {
-        // TODO: set up everything to do with the player object and 1st person camera?
         Material playerMaterial = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         playerMaterial.setColor("Color", ColorRGBA.Blue);
 
@@ -37,6 +55,8 @@ public class SpaceshipsApp extends SimpleApplication {
 
         player = new Geometry("player", playerMesh);
         player.setMaterial(playerMaterial);
+        player.setLocalTranslation(PLAYER_INITIAL_POSITION);
+        // TODO: attach the cam to the player?
 
         rootNode.attachChild(player);
     }
